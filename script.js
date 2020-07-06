fc21842513ab82f9808aaa7188367181e041f9f5
Matt_Bundner <mbundner13@gmail.com>
2020-07-06 14:14:36 -0400

    added first event listeners

--- script.js
+++ script.js
@@ -8,18 +8,42 @@ let board = document.querySelector(".board")
 
 //Game status variables
 let gameIsRunning = true
-let xIsNext = true
+let xIsUp = true
 
 //Event Handler Functions
 function resetGame(e){
-    e.preventDefault
-    console.log(e)
+    e.preventDefault()
+
 }
+function squareChoice(e){
+    e.preventDefault()
+    let specificSquare = e.target.classList[1]
+    let classList = e.target.classList
+    console.log(specificSquare)
+    console.log(classList)
+    
+    if (classList[2] === "X" || classList[2] === "O"){
+        return;
+    }
+    
+    if (xIsUp){
+        classList.add("X")
+        xIsUp === false
+    }else{
+        classList.add("O")
+        xIsUp === false
+    }
+
+}
+
+
+
 
 
 //Event listeners
 resetButton.addEventListener("click", resetGame)
 
 for(let i=0; i<squares.length; i++){
-    squares[i].addEventListener("click")
+    //console.log(squares[i])
+    squares[i].addEventListener("click", squareChoice)
 }
