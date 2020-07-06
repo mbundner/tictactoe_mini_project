69030f523b06e92e01d9502a8880de996fa74e94
Matt_Bundner <mbundner13@gmail.com>
2020-07-06 13:22:10 -0400

    defined DOM variables

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
+//HTML elements
 let resetButton = document.querySelector(".button")
-//console.log(resetButton)
+let squares = document.querySelectorAll(".squares")
 let board = document.querySelector(".board")
+//console.log(resetButton)
+//console.log(squares)
+//console.log(board)
 
-    
+//Game status variables
+let gameIsRunning = true
+let xIsNext = true
+
+//Event Handler Functions
+function resetGame(e){
+    e.preventDefault
+    console.log(e)
+}
+
+
+//Event listeners
+resetButton.addEventListener("click", resetGame)
+
+for(let i=0; i<squares.length; i++){
+    squares[i].addEventListener("click")
+}
